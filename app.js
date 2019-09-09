28056e2bedd531d061143c485fbce748fd85fa8a
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2019-09-10 00:54:29 +0200

    ADD pug templates.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+var loginRouter = require("./routes/login");
 var usersRouter = require("./routes/users");
 
 var app = express();
@@ -13,13 +14,23 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Using Morgan, logs all HTTP requests. The "dev" option gives it a specific styling.
+// https://github.com/expressjs/morgan#dev
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Add request URL to the `locals` property of the response
+// so that it can be accessed by the nav bar (to determine the active page)
+app.use(function(req, res, next){
+  res.locals.path = req.path;
+  next();
+})
+
 app.use("/", indexRouter);
+app.use("/login", loginRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Google Discord Server"
+  });
 });
 
 module.exports = router;

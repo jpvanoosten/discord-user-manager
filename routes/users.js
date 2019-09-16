8323d92a649c7c54b9ed0170dddaefc3a87fad7a
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2019-09-16 23:33:25 +0200

    ADD Query filter to users page.

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,39 @@
 // const createError = require("http-errors");
 const debug = require("debug")("discord-user-manager:users");
 const express = require("express");
+const Sequelize = require("sequelize");
 const models = require("../models");
 const router = express.Router();
+
+const OR = Sequelize.Op.or;
+const LIKE = Sequelize.Op.like;
+const SUBSTRING = Sequelize.Op.substring;
 
 /* GET users listing. */
 router.get("/", async (req, res) => {
   // Get the query parameters
-  const where = req.query.q;
+  const search = req.query.q;
   const limit = Math.max(parseInt(req.query.limit || 50), 1);
   const offset = parseInt(req.query.offset || 0);
   const order = req.query.order;
+
+  let where;
+  if (search) {
+    where = {
+      [OR]: [
+        {
+          username: {
+            [SUBSTRING]: search
+          }
+        },
+        {
+          discordId: {
+            [SUBSTRING]: search
+          }
+        }
+      ]
+    };
+  }
 
   debug("Querying users");
   // Get a list of users
@@ -30,7 +53,8 @@ router.get("/", async (req, res) => {
     numUsers,
     users,
     limit,
-    offset
+    offset,
+    search
   });
 });
 

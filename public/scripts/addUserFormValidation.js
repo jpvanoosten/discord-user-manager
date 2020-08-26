772699285478f6d99a495ba8b02e5a261aebf23f
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2020-08-26 17:49:21 +0200

    Add form validation to the Add User form.

--- public/scripts/addUserFormValidation.js
+++ public/scripts/addUserFormValidation.js
@@ -1,36 +1,37 @@
 $(document).ready(function () {
-  // $("#addUserModal form").validate({
-  //   debug: true,
-  //   rules: {
-  //     username: {
-  //       required: true,
-  //       minlength: 4,
-  //     },
-  //     name: "required",
-  //     password: {
-  //       required: true,
-  //       minlength: 8,
-  //     },
-  //   },
-  //   messages: {
-  //     username: {
-  //       required: "Username is required.",
-  //       minlength: jQuery.validator.format("Username must be at least {0} characters long."),
-  //     },
-  //     name: "Please specify the full name.",
-  //     password: {
-  //       required: "Password cannot be empty.",
-  //       minlength: jQuery.validator.format("Password must be at least {0} charaters long."),
-  //     },
-  //   },
-  //   submitHandler: function (form) {
-  //     console.log("Form submitted.");
-  //     $(form).submit();
-  //   },
-  //   // invalidHandler: function (event, _validator) {
-  //   //   console.log("Form contains invlaid data.");
-  //   //   event.preventDefault();
-  //   //   event.stopPropagation();
-  //   // },
-  // });
+  $("#addUserModal form").validate({
+    rules: {
+      username: {
+        required: true,
+        minlength: 4,
+      },
+      name: {
+        required: true,
+        minlength: 4,
+      },
+      password: {
+        required: true,
+        minlength: 8,
+      },
+    },
+    messages: {
+      username: {
+        required: "Username is required.",
+        minlength: jQuery.validator.format("Username must be at least {0} characters long."),
+      },
+      name: {
+        required: "Name is required.",
+        minlength: jQuery.validator.format("Name must be at least {0} characters long."),
+      },
+      password: {
+        required: "Password cannot be empty.",
+        minlength: jQuery.validator.format("Password must be at least {0} charaters long."),
+      },
+    },
+    submitHandler: function (form) {
+      console.log("Form submitted.");
+      form.submit();
+      $("#addUserModal").modal("hide");
+    },
+  });
 });

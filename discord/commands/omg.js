a77600718b406c494acb8bc0975449ce6d6244f3
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2020-11-04 23:57:32 +0100

    Add !omg command.

---
+++ discord/commands/omg.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "omg",
+  aliases: ["god"],
+  description: "Check to see if this bot is a God.",
+  cooldown: 5,
+  execute(message) {
+    message.reply("Yes?");
+  },
+};
